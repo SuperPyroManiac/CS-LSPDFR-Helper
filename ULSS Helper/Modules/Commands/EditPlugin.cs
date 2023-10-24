f43f4d7d501a636223e4c4760816baab65475ad3
HeLau1337 <Hendrik.Laudemann+git@gmail.com>
2023-10-24 12:18:32 +0200

    improved edit commands by making the state/level parameters optional; if they are null, the state/level won't be changed

--- ULSS Helper/Modules/Commands/EditPlugin.cs
+++ ULSS Helper/Modules/Commands/EditPlugin.cs
@@ -7,9 +7,11 @@ namespace ULSS_Helper.Modules.Commands;
 public class EditPlugin : ApplicationCommandModule
 {
     [SlashCommand("EditPlugin", "Edits a plugin in the database!")]
-    public async Task Cmd(InteractionContext ctx, 
+    public async Task Cmd(
+        InteractionContext ctx, 
         [Option("Name", "Plugins name as shown in the log!")] string pN, 
-        [Option("State", "Plugin state, LSPDFR, EXTERNAL, BROKEN, LIB")] State pS)
+        [Option("New_State", "Plugin state, LSPDFR, EXTERNAL, BROKEN, LIB")] State? pS=null
+    )
     {
         if (ctx.Member.Roles.All(role => role.Id != Settings.GetTSRole()))
         {
@@ -26,7 +28,7 @@ public class EditPlugin : ApplicationCommandModule
         var plugin = DatabaseManager.LoadPlugins().FirstOrDefault(x => x.Name == pN);
 
         Program.PlugName = plugin.Name;
-        Program.PlugState = pS;
+        if (pS != null) Program.PlugState = (State) pS;
         
         DiscordInteractionResponseBuilder modal = new();
         modal.WithTitle($"Editing {Program.PlugName} as {Program.PlugState.ToString()}").WithCustomId("edit-plugin").AddComponents(
