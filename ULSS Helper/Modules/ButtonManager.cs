3644d26e83a7588c9e26e1c611728fe84eefb72e
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2023-10-28 22:20:31 -0600

    Update ButtonManager.cs

--- ULSS Helper/Modules/ButtonManager.cs
+++ ULSS Helper/Modules/ButtonManager.cs
@@ -7,7 +7,14 @@ public class ButtonManager
 {
     internal static async Task OnButtonPress(DiscordClient s, ComponentInteractionCreateEventArgs e)
     {
+        //Select log buttons
+        //TODO: make the dang thing
+        
+        //RPH log reader buttons
         if (e.Id is "send" or "send2") await Messages.RphLogAnalysisMessages.SendMessageToUser(e);
         if (e.Id == "info") await Messages.RphLogAnalysisMessages.SendDetailedInfoMessage(e);
+        
+        //ELS log reader buttons
+        //TODO: make the dang thing
     }
 }
