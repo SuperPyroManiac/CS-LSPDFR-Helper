4ff823dcc7869dc3e3fc92e313c54d64c7433e95
Hendrik <26305045+HeLau1337@users.noreply.github.com>
2023-11-08 23:23:33 +0100

    allow RPH and ELS logs (in all cases)

--- ULSS Helper/Events/ContextMenu.cs
+++ ULSS Helper/Events/ContextMenu.cs
@@ -23,15 +23,23 @@ internal class ContextMenu : ApplicationCommandModule
             await e.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, emb);
             return;
         }
+
+        //===//===//===////===//===//===////===//Attachment Checks/===////===//===//===////===//===//===//
+        _file = null;
+        List<string> acceptedFileNames = new(new string[]{
+            "RagePluginHook",
+            "ELS"
+        });
+        string acceptedFileNamesString = string.Join(" or ", acceptedFileNames);
+        string acceptedLogFileNamesString = "`" + string.Join(".log` or `", acceptedFileNames) + ".log`";
         try
         {
-            //===//===//===////===//===//===////===//Attachment Checks/===////===//===//===////===//===//===//
             switch (e.TargetMessage.Attachments.Count)
             {
                 case 0:
                     var emb = new DiscordInteractionResponseBuilder();
                     emb.IsEphemeral = true;
-                    emb.AddEmbed(BasicEmbeds.Error("No attachment found. There needs to be a RPH or ELS log file attached to the message!"));
+                    emb.AddEmbed(BasicEmbeds.Error($"No attachment found. There needs to be a {acceptedFileNamesString} log file attached to the message!"));
                     await e.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, emb);
                     return;
                 case 1:
@@ -40,7 +48,7 @@ internal class ContextMenu : ApplicationCommandModule
                 case > 1:
                     foreach(DiscordAttachment attachment in e.TargetMessage.Attachments)
                     {
-                        if (attachment.Url.Contains("RagePluginHook.log"))
+                        if (acceptedFileNames.Any(attachment.Url.Contains))
                         {
                             _file = attachment.Url;
                             break;
@@ -50,7 +58,7 @@ internal class ContextMenu : ApplicationCommandModule
                     {
                         var emb2 = new DiscordInteractionResponseBuilder();
                         emb2.IsEphemeral = true;
-                        emb2.AddEmbed(BasicEmbeds.Error("There is no file named `RagePluginHook.log!`"));
+                        emb2.AddEmbed(BasicEmbeds.Error($"There is no log file named {acceptedLogFileNamesString} file attached!"));
                         await e.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, emb2);
                         return;
                     }
@@ -60,15 +68,15 @@ internal class ContextMenu : ApplicationCommandModule
             {
                 var emb = new DiscordInteractionResponseBuilder();
                 emb.IsEphemeral = true;
-                emb.AddEmbed(BasicEmbeds.Error("Failed to load `RagePluginHook.log`!"));
+                emb.AddEmbed(BasicEmbeds.Error("Failed to load attached file!"));
                 await e.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, emb);
                 return;
             }
-            if (!_file.Contains("RagePluginHook") && !_file.Contains("ELS"))
+            if (!acceptedFileNames.Any(_file.Contains))
             {
                 var emb = new DiscordInteractionResponseBuilder();
                 emb.IsEphemeral = true;
-                emb.AddEmbed(BasicEmbeds.Error("This file is not named `RagePluginHook.log` or `ELS.log`!"));
+                emb.AddEmbed(BasicEmbeds.Error($"This file is not named {acceptedLogFileNamesString}!"));
                 await e.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, emb);
                 return;
             }
