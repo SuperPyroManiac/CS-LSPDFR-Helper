6409d85e85a76f9e6e165863bf95a431da6f25fb
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2023-11-15 22:06:59 -0700

    Update AddTS.cs

--- ULSS Helper/Commands/AddTS.cs
+++ ULSS Helper/Commands/AddTS.cs
@@ -13,13 +13,13 @@ public class AddTS : ApplicationCommandModule
     public async Task AddTSCmd(InteractionContext ctx, [Option("ID", "User discord ID")] string id,
         [Option("Allow", "Allow access to the bot commands!")] bool allow)
     {
-        Console.WriteLine("-1");
+Console.WriteLine("-1");
         if (ctx.Member.Id != 339550607847194624)
         {
             await ctx.CreateResponseAsync(embed: BasicEmbeds.Error("You do not have permission for this!"));
             return;
-        }
-        Console.WriteLine("0");
+        } 
+Console.WriteLine("0");
 
         var allowint = 0;
         if (allow) allowint = 1;
