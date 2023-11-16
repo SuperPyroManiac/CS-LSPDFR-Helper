debbdeccbbf30607a94b16a6564d089ae78eebcc
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2023-11-15 22:24:57 -0700

    Use string instead of int32

--- ULSS Helper/Commands/AddTS.cs
+++ ULSS Helper/Commands/AddTS.cs
@@ -13,27 +13,20 @@ public class AddTS : ApplicationCommandModule
     public async Task AddTSCmd(InteractionContext ctx, [Option("ID", "User discord ID")] string id,
         [Option("Allow", "Allow access to the bot commands!")] bool allow)
     {
-Console.WriteLine("-1");
         if (ctx.Member.Id != 339550607847194624)
         {
             await ctx.CreateResponseAsync(embed: BasicEmbeds.Error("You do not have permission for this!"));
             return;
         } 
-Console.WriteLine("0");
 
         var allowint = 0;
         if (allow) allowint = 1;
-Console.WriteLine("1");
-        var ts = new TS
-        {
-            ID = int.Parse(id),
-            Username = ctx.Guild.GetMemberAsync(ulong.Parse(id)).Result.Username,
-            View = 0,
-            Allow = allowint
-        };
-Console.WriteLine("2");
+        var ts = new TS();
+        ts.ID = id;
+        ts.Username = ctx.Guild.GetMemberAsync(ulong.Parse(id)).Result.Username;
+        ts.View = 0;
+        ts.Allow = allowint;
         Database.AddTS(ts);
-Console.WriteLine("3");
         await ctx.CreateResponseAsync(BasicEmbeds.Warning($"**Added new TS {ts.Username} with user ID: {ts.ID}**"));
         Logging.SendLog(ctx.Interaction.Channel.Id, ctx.Interaction.User.Id,
             BasicEmbeds.Warning($"**Added new TS {ts.Username} with user ID: {ts.ID}**"));
--- ULSS Helper/Objects/TS.cs
+++ ULSS Helper/Objects/TS.cs
@@ -2,7 +2,7 @@
 
 public class TS
 {
-    public int ID { get; set; }
+    public string ID { get; set; }
     public string Username { get; set; }
     public int View { get; set; }
     public int Allow { get; set; }
