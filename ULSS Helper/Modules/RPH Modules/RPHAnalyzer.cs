daf9b9e6bf54e6d202ed5485dc8452a74486393d
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2023-11-23 09:19:25 -0700

    Revert "Make a mess of error 1"
    This reverts commit befd34fc129196892ab42a09d18982576df3cf82.

--- ULSS Helper/Modules/RPH Modules/RPHAnalyzer.cs
+++ ULSS Helper/Modules/RPH Modules/RPHAnalyzer.cs
@@ -175,25 +175,19 @@ public class RPHAnalyzer
             }
         }
         
-        var libregex = new Regex(@"(?:\[.+\]) (?!ERROR: Could not load plugin from|SuperEvents: Registering event -|Error while loading plugin)(?!.*(Grammar)).+: (?!Creating|Starting dependency check for|Type .SlimDX.+)(?:.+)\s* \W?(?!ScriptHookVDotNet3|LemonUI\.SHVDN3|ERROR\] there was an error while trying to access plugin:)(.+), Version=.+, Culture=.+PublicKeyToken=.+");
+        var libregex = new Regex(@"(?:\[.+\]) (?!ERROR: Could not load plugin from|SuperEvents: Registering event -|Error while loading plugin|SuperEvents: Loading (?:\w+\s+)+)(?!.*(Grammar|Assembly: |CalloutInterface: \[ERROR\]))(?:.+:) (?!Creating|Starting dependency check for|Type .SlimDX)(?:\w+\s+)+(?:'|"")?(?!ScriptHookVDotNet3|LemonUI\.SHVDN3|ERROR\] there was an error while trying to access plugin:)*(.+), Version=.+, Culture=.+PublicKeyToken=");
         var libmatch = libregex.Matches(wholeLog);
         foreach (Match match in libmatch)
         {
-            foreach (var plugg in pluginData)
+            if (!log.MissingDepend.Any(x => x.Name.Equals(match.Groups[2].Value)))
             {
-                if (match.Value.Contains(plugg.Name + ","))
+                var newLib = new Plugin
+                { Name = match.Groups[2].Value, State = "LIB", DName = match.Groups[2].Value};
+                foreach (var plugin in pluginData.Where(plugin => plugin.Name.Equals(newLib.Name)))
                 {
-                    if (!log.MissingDepend.Any(x => x.Name.Equals(match.Groups[2].Value)))
-                    {
-                        var newLib = new Plugin
-                            { Name = match.Groups[2].Value, State = "LIB", DName = match.Groups[2].Value };
-                        foreach (var plugin in pluginData.Where(plugin => plugin.Name.Equals(newLib.Name)))
-                        {
-                            newLib.DName = plugin.DName;
-                            newLib.Link = plugin.Link;
-                            log.MissingDepend.Add(newLib);
-                        }
-                    }
+                    newLib.DName = plugin.DName;
+                    newLib.Link = plugin.Link;
+                    log.MissingDepend.Add(newLib);
                 }
             }
         }
