3aeb813df21b5a065e74f4c8a9d765cc29f46a7a
Hendrik Laudemann <26305045+HeLau1337@users.noreply.github.com>
2023-12-01 05:18:35 +0100

    send message when scheduled tasks run

--- ULSS Helper/Timer.cs
+++ ULSS Helper/Timer.cs
@@ -1,4 +1,6 @@
 using System.Timers;
+using DSharpPlus.Entities;
+using ULSS_Helper.Messages;
 using ULSS_Helper.Objects;
 
 namespace ULSS_Helper;
@@ -8,20 +10,33 @@ internal class Timer
     public const int HourInterval = 3;
     internal static void StartTimer()
     {
-        var aTimer = new System.Timers.Timer(60 * 60 * 1000 * HourInterval); //3 hours
-        aTimer.Elapsed += OnTimedEvent;
-        aTimer.Start();
+        var timer = new System.Timers.Timer(TimeSpan.FromHours(HourInterval).TotalMilliseconds);
+        timer.Elapsed += OnTimedEvent;
+        timer.Start();
     }
     private static void OnTimedEvent(object source, ElapsedEventArgs e)
     {
-        //Update Checker
+        DiscordEmbedBuilder embed = BasicEmbeds.Info(
+            msg: $"Running scheduled tasks now\n"
+                + $"Current schedule: every {HourInterval} hours\n"
+                + $"Tasks:\n"
+                + $"- update LSPDFR plugin versions in DB using the lcpdfr.com API\n"
+                + $"- create a DB backup\n"
+                + $"- clear expired entries in the bot's cache\n",
+            bold: true
+        );
+        new DiscordMessageBuilder()
+	        .WithEmbed(embed)
+	        .SendAsync(Program.Client.GetChannelAsync(Program.Settings.Env.TsBotLogChannelId).Result);
+
+        // Update LSPDFR plugin versions in DB using the lcpdfr.com API
         var th = new Thread(Database.UpdatePluginVersions);
         th.Start();
         
-        //Backup DB
+        // Backup DB
         File.Copy(Program.Settings.DbPath, Settings.GenerateNewFilePath(FileType.DB_BACKUP));
 
-        //Clean Cache
+        // Clean Cache
         Task.Run(() => Program.Cache.RemoveExpiredCacheEntries(TimeSpan.FromHours(HourInterval)));
     }
 }
