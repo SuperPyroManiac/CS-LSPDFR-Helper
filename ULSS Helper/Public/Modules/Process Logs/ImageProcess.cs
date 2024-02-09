85dc41c97f2fab72d86ee18f1ad65bd67db44f23
Hendrik <26305045+HeLau1337@users.noreply.github.com>
2024-02-09 20:30:39 +0100

    added ts-bot-log messages, better handling of 'no text/errors found', improved styling

--- ULSS Helper/Public/Modules/Process Logs/ImageProcess.cs
+++ ULSS Helper/Public/Modules/Process Logs/ImageProcess.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using System.Text.RegularExpressions;
 using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using Tesseract;
+using ULSS_Helper.Messages;
+using ULSS_Helper.Objects;
 
 namespace ULSS_Helper.Public.Modules.Process_Logs;
 public class ImageProcess
@@ -17,7 +20,7 @@ public class ImageProcess
             // 3. move the downloaded "eng.traineddata" file into the created "tessdata" folder
             var engine = new TesseractEngine(@".\tessdata", "eng");
             
-            DiscordEmbedBuilder embed = new()
+            DiscordEmbedBuilder publicEmbed = new()
             {
                 Description = $"## __ULSS AutoHelper__",
                 Color = new DiscordColor(243, 154, 18),
@@ -35,29 +38,74 @@ public class ImageProcess
                     if (response.IsSuccessStatusCode)
                     {
                         byte[] imageData = await response.Content.ReadAsByteArrayAsync();
-                        Console.WriteLine("Image downloaded successfully!");
-
                         var image = Pix.LoadFromMemory(imageData);
                         var page = engine.Process(image);
 
                         var text = page.GetText().Trim();
+                        var textNoLineBreaks = text.Replace("\n", " ").Replace("\r", " ");
                         
-                        embed.AddField("I detected the following text in the uploaded image:", $"```{text}```");
-                        
+                        if (String.IsNullOrEmpty(text))
+                        {
+                            var logNoTextEmbed = BasicEmbeds.Info("No text recognized in uploaded image");
+                            logNoTextEmbed.AddField("Image:", $"[Link](<{attachment.Url}>)");
+                            Logging.SendLog(ctx.Message.Channel.Id, ctx.Message.Author.Id, logNoTextEmbed);
+                            return;
+                        }
+
+                        var matchedErrors = new List<Error>();
                         foreach (var error in Database.LoadErrors().Where(error => error.Level == "AUTO"))
                         {
                             var errregex = new Regex(error.Regex);
-                            var errmatch = errregex.Match(text);
+                            var errmatch = errregex.Match(textNoLineBreaks);
                             if (errmatch.Success)
-                                embed.AddField($"My suggested solution (ID: {error.ID}):",$"{error.Solution}");
+                            {
+                                matchedErrors.Add(error);
+                                
+                                // response to public user
+                                publicEmbed.AddField(
+                                    "\r\nCommon issue detected in uploaded image:", 
+                                    $"> {errmatch.Value}"
+                                );
+                                publicEmbed.AddField($"Suggested troubleshooting steps (ID {error.ID}):", $"> {error.Solution.Replace("\n", "\n> ")}\r\n");
+                            }
                         }
 
-                        messageBuilder.AddEmbed(embed);
+                        
+                        // ts-bot-log
+                        var logEmbedContentBuilder = new StringBuilder("**__Uploaded image was processed__**\r\n\r\n");
+                        logEmbedContentBuilder.Append($"Sender: <@{ctx.Message.Author.Id}>\r\n");
+                        logEmbedContentBuilder.Append($"Channel: <#{ctx.Message.Channel.Id}>\r\n");
+                        logEmbedContentBuilder.Append($"Image: [{attachment.FileName}]({attachment.Url}) ({attachment.FileSize / 1000}KB)\r\n");
+                        logEmbedContentBuilder.Append($"Recognized text: ```{text}```\r\n");
+                        if (matchedErrors.Count > 0)
+                        {
+                            var matchedErrorIds = matchedErrors.Select(matchedError => matchedError.ID).ToList();
+                            logEmbedContentBuilder.Append($"Matched with error IDs: {string.Join(", ", matchedErrorIds)}");
+                            
+                        }
+                        else
+                        {
+                            publicEmbed.AddField(
+                                $"No common issues found in the text within the uploaded image. Try the following:\n", 
+                                "- if you think your screenshot contains some kind of text or error message that corresponds to a common issue that I know about...\n" +
+                                " - try creating a higher quality screenshot (no phone pictures of your computer screen), use the PRINT-SCREEN key or Windows Key + Shift + S.\n" +
+                                " - try cropping the screenshot before uploading it here so it only shows the error message and no other unrelated text on your screen.\n" +
+                                "- if the above applies to you but you already tried the suggested steps, feel free to request help from a human.\n" +
+                                "" +
+                                "\r\n"
+                            );
+                            logEmbedContentBuilder.Append($"Matched with error IDs: None");
+                        }
+                        
+                        var logEmbed = BasicEmbeds.Info(logEmbedContentBuilder.ToString());
+                        Logging.SendPubLog(logEmbed);
+                        
+                        messageBuilder.AddEmbed(publicEmbed);
                         await ctx.Message.RespondAsync(messageBuilder);
                     }
                     else
                     {
-                        Console.WriteLine("Error downloading image: " + response.StatusCode);
+                        throw new Exception("Error downloading image: " + response.StatusCode);
                     }
                 }
             }
