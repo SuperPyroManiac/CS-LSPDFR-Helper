787c140c869fd26cf41b3f353c26253ddde9814f
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-02-23 17:05:06 -0700

    Update RPHSpecialErrors.cs

--- ULSS Helper/Modules/RPH Modules/RPHSpecialErrors.cs
+++ ULSS Helper/Modules/RPH Modules/RPHSpecialErrors.cs
@@ -115,7 +115,7 @@ public class RPHSpecialErrors
                 foreach (Capture capture in match.Groups[i].Captures)
                 {
                     if (causedCrash.Any(x => x.Name.Equals(capture.Value))) continue;
-                    foreach (var plugin in pluginData.Where(plugin => plugin.Name.Equals(capture.Value)))
+                    foreach (var plugin in pluginData.Where(plugin => plugin.Name.Equals(capture.Value) && plugin.State is "LSPDFR" or "EXTERNAL"))
                     {
                         if (!causedCrash.Contains(plugin))
                         {
