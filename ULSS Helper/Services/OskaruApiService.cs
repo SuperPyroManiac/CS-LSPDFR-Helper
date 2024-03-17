0b1ae59126cae88af7887e0ac9969f833cc9c098
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-03-16 18:36:43 -0600

    Update OskaruApiService.cs

--- ULSS Helper/Services/OskaruApiService.cs
+++ ULSS Helper/Services/OskaruApiService.cs
@@ -6,7 +6,6 @@ namespace ULSS_Helper.Services;
 
 record AnalyseImageLinkRequest
 {
-    Console.WriteLine("1hukilhku");
     public String downloadLink { get; set; }
     public String caption { get; set; }
 }
@@ -36,6 +35,7 @@ public class OskaruApiService
             downloadLink = imageUrl,
             caption = caption
         };
+        Console.WriteLine("o2");
         var serializeOptions = new JsonSerializerOptions
         {
             WriteIndented = true,
