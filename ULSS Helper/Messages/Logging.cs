1c7acd436ee472d2fb3ea21cb6d87916c5e8396b
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-03-18 11:48:22 -0600

    Check char count for errors
    Error logging shouldnt throw errors

--- ULSS Helper/Messages/Logging.cs
+++ ULSS Helper/Messages/Logging.cs
@@ -8,7 +8,14 @@ internal class Logging
     internal static async Task ErrLog(string e)
     {
         var tsBotLogCh = await Program.Client.GetChannelAsync(Program.Settings.Env.TsBotLogChannelId);
-        await new DiscordMessageBuilder().WithContent($"### Error Detected\r\n```{e}```").SendAsync(tsBotLogCh); //TODO: Check msg size
+
+        if (e.Length >= 4000)
+        {
+            var ee = e.Substring(0, 3800);
+            await new DiscordMessageBuilder().WithContent($"### Error Detected\r\n```{ee}...+more```\r\n**Over character limit! See log for details!").SendAsync(tsBotLogCh);
+            return;
+        }
+        await new DiscordMessageBuilder().WithContent($"### Error Detected\r\n```{e}```").SendAsync(tsBotLogCh);
     }
     
     internal static async Task SendLog(ulong chLink, ulong msgSender, DiscordEmbedBuilder e)
