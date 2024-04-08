3db8c9ce3644cb96d42c97fe08515ad02fe733ed
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-04-08 15:27:13 -0600

    Temp logging

--- ULSS Helper/Public/AutoHelper/Modules/Case Functions/CheckCases.cs
+++ ULSS Helper/Public/AutoHelper/Modules/Case Functions/CheckCases.cs
@@ -1,3 +1,5 @@
+using ULSS_Helper.Messages;
+
 namespace ULSS_Helper.Public.AutoHelper.Modules.Case_Functions;
 
 internal class CheckCases
@@ -10,6 +12,7 @@ internal class CheckCases
             if (th.ThreadMetadata.IsArchived) continue;
             foreach (var ac in Program.Cache.GetCasess().Where(c => c.ChannelID.Equals(th.Id.ToString())))
             {
+                await Logging.SendPubLog(BasicEmbeds.Error("Detected broken thread\r\n" + ac.ChannelID, true));
                 if (ac.Solved == 1 || ac.Timer == 0) await CloseCase.Close(ac);
             }
         }
