3430e022258dba78d1e4c240af438f0d8fe7aa81
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-04-08 15:37:05 -0600

    Update CheckCases.cs

--- ULSS Helper/Public/AutoHelper/Modules/Case Functions/CheckCases.cs
+++ ULSS Helper/Public/AutoHelper/Modules/Case Functions/CheckCases.cs
@@ -9,8 +9,8 @@ internal class CheckCases
         var parentCh = await Program.Client.GetChannelAsync(Program.Settings.Env.AutoHelperChannelId);
         foreach (var th in parentCh.Threads)
         {
-            if (th.ThreadMetadata.IsArchived) continue;
             await Logging.SendPubLog(BasicEmbeds.Error("Detected broken thread\r\n" + th.Id.ToString(), true));
+            if (th.ThreadMetadata.IsArchived) continue;
             foreach (var ac in Program.Cache.GetCasess())
             {
                 if (!ac.ChannelID.Equals(th.Id.ToString())) continue;
