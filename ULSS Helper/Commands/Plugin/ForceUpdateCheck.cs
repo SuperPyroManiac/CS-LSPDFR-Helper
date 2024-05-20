0a86688e862250fbaebad539c532c3d1046448ce
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-05-19 22:31:07 -0600

    Adjust formatting

--- ULSS Helper/Commands/Plugin/ForceUpdateCheck.cs
+++ ULSS Helper/Commands/Plugin/ForceUpdateCheck.cs
@@ -17,7 +17,7 @@ public class ForceUpdateCheck : ApplicationCommandModule
         var th = new Thread(Database.UpdatePluginVersions);
         th.Start();
         
-        await ctx.CreateResponseAsync(bd.AddEmbed(BasicEmbeds.Success("Started DB updater!\r\nPlease allow a few minutes for everything to update!", true)));
+        await ctx.CreateResponseAsync(bd.AddEmbed(BasicEmbeds.Success("__Started DB updater!__\r\nPlease allow a few minutes for everything to update!", true)));
         await Logging.SendLog(ctx.Interaction.Channel.Id, ctx.Interaction.User.Id, BasicEmbeds.Info("__Forced DB updater to run!__", true));
     }
 }
