b4ab890eac8ac3914feabdc9cfcf7869e1aab41c
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-06-15 08:57:56 -0600

    Global settings DB setup

--- LSPDFR Helper/Functions/DBManager.cs
+++ LSPDFR Helper/Functions/DBManager.cs
@@ -9,12 +9,29 @@ internal class DbManager
 {
     private static readonly string ConnStr = $"Server={Program.Settings.Env.DbServer};User ID={Program.Settings.Env.DbUser};Password={Program.Settings.Env.DbPass};Database={Program.Settings.Env.DbName}";
 
-    internal static Dictionary<string, string> GetGlobalSettings()
+    internal static GlobalSettings GetGlobalSettings()
     {
         using IDbConnection cnn = new MySqlConnection(ConnStr);
         var dict = cnn.Query("select * from GlobalValues").ToDictionary(
             row => (string)row.Name,
             row => (string)row.Value);
-        return dict;
+        return new GlobalSettings
+        {
+            GTAVer = dict["GTAVer"],
+            RPHVer = dict["RPHVer"],
+            LSPDFRVer = dict["LSPDFRVer"],
+            SHVVer = dict["SHVVer"],
+            AHStatus = bool.Parse(dict["AHStatus"]),
+            ServerId = ulong.Parse(dict["ServerId"]),
+            TsRoleId = ulong.Parse(dict["TsRoleId"]),
+            TsIconUrl = dict["TsIconUrl"],
+            AutoHelperChId = ulong.Parse(dict["AutoHelperChId"]),
+            SupportChId = ulong.Parse(dict["SupportChId"]),
+            MonitorChId = ulong.Parse(dict["MonitorChId"]),
+            BotLogChId = ulong.Parse(dict["BotLogChId"]),
+            PublicLogChId = ulong.Parse(dict["PublicLogChId"]),
+            ReportChId = ulong.Parse(dict["ReportChId"]),
+            StaffContactChId = ulong.Parse(dict["StaffContactChId"])
+        };
     }
 }
