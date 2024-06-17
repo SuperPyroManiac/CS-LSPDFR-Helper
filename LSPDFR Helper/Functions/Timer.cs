1521f8189a170c174bd4f8f08a3268c7cf84ca6e
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-06-17 15:30:26 -0600

    Add Timer.cs

---
+++ LSPDFR Helper/Functions/Timer.cs
@@ -0,0 +1,39 @@
+using System.Timers;
+
+namespace LSPDFR_Helper.Functions;
+
+internal static class Timer
+{
+    internal static void Start()
+    {
+        var aTimer = new System.Timers.Timer(TimeSpan.FromHours(1));
+        aTimer.Elapsed += HourlyTimer;
+        aTimer.Start();
+
+        var bTimer = new System.Timers.Timer(TimeSpan.FromSeconds(30));
+        bTimer.Elapsed += ShortTimer;
+        bTimer.Start();
+    }
+
+    private static void HourlyTimer(object _, ElapsedEventArgs e)
+    {
+        //Update Checker
+        // var th = new Thread(Database.UpdatePluginVersions);
+        // th.Start();
+    }
+
+    private static void ShortTimer(object _, ElapsedEventArgs e)
+    {
+        //Clean & Update Caches
+        //await Program.Cache.RemoveExpiredCacheEntries(TimeSpan.FromMinutes(5));
+        //await CaseMonitor.UpdateMonitor();
+        // Program.Cache.UpdatePlugins(Database.LoadPlugins());
+        // Program.Cache.UpdateErrors(Database.LoadErrors());
+        // Program.Cache.UpdateCases(Database.LoadCases());
+        // Program.Cache.UpdateUsers(Database.LoadUsers());
+        
+        //Verify Cases & Users
+        //await Task.Run(CheckUsers.Validate);
+        //await Task.Run(CheckCases.Validate);
+    }
+}
