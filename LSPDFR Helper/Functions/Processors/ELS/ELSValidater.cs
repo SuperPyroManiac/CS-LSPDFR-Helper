96a463f0f87d818b85381058292c1c5a29cdf2aa
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-06-30 21:08:47 -0600

    Better performance with GeneratedRegex

--- LSPDFR Helper/Functions/Processors/ELS/ELSValidater.cs
+++ LSPDFR Helper/Functions/Processors/ELS/ELSValidater.cs
@@ -3,28 +3,28 @@ using LSPDFR_Helper.CustomTypes.LogTypes;
 
 namespace LSPDFR_Helper.Functions.Processors.ELS;
 
-public class ELSValidater
+public partial class ELSValidater
 {
     public static async Task<ELSLog> Run(string attachmentUrl)
     {
         var log = new ELSLog();
         log.DownloadLink = attachmentUrl;
         var wholeLog = await new HttpClient().GetStringAsync(attachmentUrl);
-        log.ElsVersion = new Regex(@"VER\s+\[ ([0-9.]+) \]").Match(wholeLog).Groups[1].Value;
-        log.AdvancedHookVFound = new Regex(@"\s+\(OK\) AdvancedHookV\.dll file found and successfully initialized\.").Match(wholeLog).Success;
-        log.VcfContainer = new Regex(@"<VCONT> Attempting to open VCF container at: \[ \.(.*) \]\.\r\n\s+\(OK\) VCF container found, scanning for files\.", RegexOptions.Multiline).Match(wholeLog).Groups[1].Value.Replace("//", "/");
+        log.ElsVersion = ElsVersionReg().Match(wholeLog).Groups[1].Value;
+        log.AdvancedHookVFound = AdvancedHookVFoundReg().Match(wholeLog).Success;
+        log.VcfContainer = VcfContainerReg().Match(wholeLog).Groups[1].Value.Replace("//", "/");
 
-        foreach (Match match in new Regex(@"<FILEV> Found file: \[ (.*) \]\.\r\n<FILEV> Verifying vehicle model name \[ .* \]\.\r\n\s+\(ER\) Model specified is not valid, discarding file\.").Matches(wholeLog)) 
+        foreach (Match match in InvalidElsVcfFilesReg().Matches(wholeLog)) 
             if (!log.InvalidElsVcfFiles.Any(x => x.Equals(match.Groups[1].Value))) log.InvalidElsVcfFiles.Add(match.Groups[1].Value);
         
-        foreach (Match match in new Regex(@"<FILEV> Found file: \[ (.*) \]\.\r\n<FILEV> Verifying vehicle model name \[ .* \]\.\r\n\s+\(OK\) Is valid vehicle model, assigned VMID \[ .* \]\.\r\n\s+Parsing file. \*A crash before all clear indicates faulty VCF\.\*\r\n\s+VCF Description: .*\r\n\s+VCF Author: .*(\r\n\s+\(OK\) Collected data from: '\w+'\.)+\r\n\s+\(OK\) ALL CLEAR -- Configuration file processed\.").Matches(wholeLog))
+        foreach (Match match in ValidElsVcfFilesReg().Matches(wholeLog))
             if (!log.ValidElsVcfFiles.Any(x => x.Equals(match.Groups[1].Value)) && !log.InvalidElsVcfFiles.Any(x => x.Equals(match.Groups[1].Value)))
                 log.ValidElsVcfFiles.Add(match.Groups[1].Value);
 
-        if (new Regex(@"\s+\(OK\) Collected data from: '\w+'\.\r\n$").Match(wholeLog).Success)
-            log.FaultyVcfFile = new Regex(@"<FILEV> Found file: \[ (.+\.xml) \]\.").Matches(wholeLog)[^1].Groups[1].Value;
+        if ( DetectFaultReg().Match(wholeLog).Success)
+            log.FaultyVcfFile = FaultyVcfFileReg().Matches(wholeLog)[^1].Groups[1].Value;
 
-        var matchTotalAmountVcfs = new Regex(@"<FILEV> Vehicle information retrieved\. Loaded \[ (\d+) \] ELS-enabled model\(s\)\.").Match(wholeLog);
+        var matchTotalAmountVcfs = matchTotalAmountVcfsReg().Match(wholeLog);
         if (matchTotalAmountVcfs.Success) log.TotalAmountElsModels = int.Parse(matchTotalAmountVcfs.Groups[1].Value);
         else { log.TotalAmountElsModels = null; }
         
@@ -33,4 +33,21 @@ public class ELSValidater
 
         return log;
     }
+
+    [GeneratedRegex(@"VER\s+\[ ([0-9.]+) \]")]
+    private static partial Regex ElsVersionReg();
+    [GeneratedRegex(@"\s+\(OK\) AdvancedHookV\.dll file found and successfully initialized\.")]
+    private static partial Regex AdvancedHookVFoundReg();
+    [GeneratedRegex(@"<FILEV> Found file: \[ (.*) \]\.\r\n<FILEV> Verifying vehicle model name \[ .* \]\.\r\n\s+\(ER\) Model specified is not valid, discarding file\.")]
+    private static partial Regex InvalidElsVcfFilesReg();
+    [GeneratedRegex(@"<FILEV> Found file: \[ (.*) \]\.\r\n<FILEV> Verifying vehicle model name \[ .* \]\.\r\n\s+\(OK\) Is valid vehicle model, assigned VMID \[ .* \]\.\r\n\s+Parsing file. \*A crash before all clear indicates faulty VCF\.\*\r\n\s+VCF Description: .*\r\n\s+VCF Author: .*(\r\n\s+\(OK\) Collected data from: '\w+'\.)+\r\n\s+\(OK\) ALL CLEAR -- Configuration file processed\.")]
+    private static partial Regex ValidElsVcfFilesReg();
+    [GeneratedRegex(@"<FILEV> Found file: \[ (.+\.xml) \]\.")]
+    private static partial Regex FaultyVcfFileReg();
+    [GeneratedRegex(@"<FILEV> Vehicle information retrieved\. Loaded \[ (\d+) \] ELS-enabled model\(s\)\.")]
+    private static partial Regex matchTotalAmountVcfsReg();
+    [GeneratedRegex(@"<VCONT> Attempting to open VCF container at: \[ \.(.*) \]\.\r\n\s+\(OK\) VCF container found, scanning for files\.", RegexOptions.Multiline)]
+    private static partial Regex VcfContainerReg();
+    [GeneratedRegex(@"\s+\(OK\) Collected data from: '\w+'\.\r\n$")]
+    private static partial Regex DetectFaultReg();
 }
