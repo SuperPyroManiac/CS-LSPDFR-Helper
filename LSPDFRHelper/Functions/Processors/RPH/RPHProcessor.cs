16dde5649bdbb80cee077a337e20f004bba00e77
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-08-26 17:21:50 -0600

    900 char

--- LSPDFRHelper/Functions/Processors/RPH/RPHProcessor.cs
+++ LSPDFRHelper/Functions/Processors/RPH/RPHProcessor.cs
@@ -142,7 +142,7 @@ public class RphProcessor : SharedData
         var update = Log.Errors.Any(x => x.Level == Level.CRITICAL);
         foreach (var error in Log.Errors)
         {
-            if ( error.Solution.Length >= 935 ) error.Solution += "\r\nSolution Length too long to continue! Fix what is shown then try again!";
+            if ( error.Solution.Length >= 900 ) error.Solution += "\r\nSolution Length too long to continue! Fix what is shown then try again!";
             if (embed.Fields.Count == 20)
             {
                 embed.AddField("___```Too Much``` Overflow:___",
